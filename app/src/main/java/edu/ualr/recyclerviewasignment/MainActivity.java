eeba97834c99c76716f480d32bdf57920e66fe77
C-Azzone416 <42585558+C-Azzone416@users.noreply.github.com>
2019-11-17 22:49:41 -0600

    fragment files added

--- app/src/main/java/edu/ualr/recyclerviewasignment/MainActivity.java
+++ app/src/main/java/edu/ualr/recyclerviewasignment/MainActivity.java
@@ -1,10 +1,11 @@
 package edu.ualr.recyclerviewasignment;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.ListFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
 
 import edu.ualr.recyclerviewasignment.adapter.DeviceListAdapter;
 import edu.ualr.recyclerviewasignment.data.DataGenerator;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private DeviceListAdapter mAdapter;
+    ListFragment listFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
